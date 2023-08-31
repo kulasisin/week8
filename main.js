2520b391410e25eaded3d3afffee2998d2f53198
kulasisin <107702039@mail2.nccu.tw>
2023-09-01 02:11:57 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+// import Swiper from "swiper/bundle";
+var swiper = new Swiper(".index-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".index-mobile-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+console.log("Hello world!");
